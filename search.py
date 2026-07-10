605fd0f13cc702c12e3963197c97cc69c72b3636
Hemanth Gowda <hemanthgowdaofficial1823@gmail.com>
2026-07-11 02:10:39 +0530

    Add Google Search module
    Initial implementation of Google search function.

---
+++ search.py
@@ -0,0 +1,8 @@
+import requests
+
+def search_google(query):
+    """
+    Searches Google and returns the response.
+    This function will be connected to the Google Search API.
+    """
+    return f"Searching Google for: {query}"
