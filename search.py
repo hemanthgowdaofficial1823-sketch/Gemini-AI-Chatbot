c2f48e115b9138b29b71447b87f02847388036b0
Hemanth Gowda <hemanthgowdaofficial1823@gmail.com>
2026-07-11 18:22:46 +0530

    Delete search.py

--- search.py
+++
@@ -1,8 +0,0 @@
-def search_google(query):
-    """
-    Placeholder for Google Search API integration.
-    """
-
-    print("Searching Google...")
-
-    return f"Google Search Result for: {query}"
